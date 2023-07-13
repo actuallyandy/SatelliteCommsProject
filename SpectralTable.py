cb1905a8d882c0d639c65dd8bdece71a8456bb00
Andres <andresmortiz1@gmail.com>
2023-07-12 19:50:12 -0700

    Skeleton of required Attenuation constants

---
+++ SpectralTable.py
@@ -0,0 +1,26 @@
+#Author: Andres Ortiz
+#Description: Table for spectroscopic oxygen and water attenuation
+#Preface: This is not a good way to code this, but I am short on time and I'm copying these values from this pdf: 
+#https://www.itu.int/dms_pubrec/itu-r/rec/p/R-REC-P.676-13-202208-I!!PDF-E.pdf
+class Oxygen:
+    frequency = []
+    a1 = []
+    a2 = []
+    a3 = []
+    a4 = []
+    a5 = []
+    a6 = []
+    length = 10
+
+class Water:
+    frequency = [22.235080, 67.803960, 119.995940, 183.310087]
+    a1 = []
+    a2 = []
+    a3 = []
+    a4 = []
+    a5 = []
+    a6 = []
+    length = 10
+
+
+    
